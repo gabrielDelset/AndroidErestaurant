9dee6c767e2848b397c9db64fc76afbfda0d6a6d
gabrielDelset <gab.delset@gmail.com>
2023-03-24 14:25:53 +0100

    ajout fichier apk

--- app/src/main/java/fr/delset/androideestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/delset/androideestaurant/CategoryActivity.kt
@@ -11,6 +11,10 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 import com.android.volley.Request
+import com.google.gson.Gson
+
+
+
 
 
 
@@ -37,28 +41,44 @@ class CategoryActivity : AppCompatActivity() {
         binding.categoryRecyclerView.adapter=adapter
       //  getDishesFromServer()
     }
+
+    private fun getDishesFromServer() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://test.api.catering.bluecodegames.com/menu"
+
+        val body = JSONObject()
+        body.put("id_shop", 1)
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST, url, body,
+            { response ->
+                Log.d("CategoryActivity", "Response : $response")
+                val data = Gson().fromJson(response.toString(), Dishinfo::class.java)
+                val dishes = data.data.firstOrNull { response.category == "Plats" }?.item?.map { response.title ?: "" }
+                adapter.updateDishes(dishes as ArrayList<String>)
+            },
+            { error ->
+                Log.e("CategoryActivity", "Erreur lors de l'appel : ${error.localizedMessage}")
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
 /*
-private fun getDishesFromServer() {
-    val queue = Volley.newRequestQueue(this)
-    val url = "http://test.apicatering.bkuecodegames.com/menu"
+    private fun parseJson(response: String, categoryTitle: String): ArrayList<String> {
+        val dishes = ArrayList<String>()
 
-    val body = JSONObject()
-    body.put("id_shop",1)
-    val JsonObjectRequest = JsonObjectRequest(
-        Request.Method.POST, url, body,
-        {response ->
-            Log.d("CategoryActivity", "ça marche : $response")
-            val data =Gson().fromJson(response.toString(),DataResult::class.java)
-            val dishes = data.data.firstOrNull{it.nameFr == categoryTitle}.item.map{it.nameFr ?:""}.toList() as ArrayList
-            adapter.updateDishes(dishes)
-        },
-        {error->
-            Log.e("CategoryActivity","erreur lors de l'appel ${error.localizeMessage}")
+        val gson = Gson()
+        val dataResult = gson.fromJson(response, Dishinfo::class.java)
+
+        val categoryData = dataResult.data.find { it.nameFr == categoryTitle }
+        categoryData?.item?.forEach { item ->
+            dishes.add(item.nameFr ?: "")
         }
-    )
-    queue.add(jsonObjectRequest) //ze
 
-}*/
+        return dishes
+    }
+
+*/
 
 }
 
